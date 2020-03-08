b6db768b5391dc86f839f9faefcb4e8a39dd70a0
planetred-cc <pandeyhimanshu709@gmail.com>
2020-03-08 11:29:02 +0530

    navbar module now works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
-import { NavbarService } from './navbar/services/navbar.service';
+import { NavbarService } from './navbar/service/navbar.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app.component';
-import { NavbarComponent } from './navbar/components/navbar.component';
-import { NavbarItemComponent } from './navbar/navbar-item/components/navbar-item.component';
-import { LogoComponent } from './navbar/logo/components/logo.component';
+import { AppComponent } from './component/app.component';
 import { NavbarModule } from './navbar/navbar.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    NavbarItemComponent,
-    LogoComponent
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, NavbarModule],
   providers: [NavbarService],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -1,4 +1,5 @@
-import { NavbarItemComponent } from './components/navbar-item/components/navbar-item.component';
+import { NavbarComponent } from './component/navbar.component';
+import { NavbarItemComponent } from './navbar-item/components/navbar-item.component';
 import { LogoComponent } from './logo/components/logo.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -6,7 +7,8 @@ import { CommonModule } from '@angular/common';
 import { NavbarRoutingModule } from './navbar-routing.module';
 
 @NgModule({
-  declarations: [LogoComponent, NavbarItemComponent],
-  imports: [CommonModule, NavbarRoutingModule]
+  declarations: [LogoComponent, NavbarItemComponent, NavbarComponent],
+  imports: [CommonModule, NavbarRoutingModule],
+  exports: [NavbarComponent]
 })
 export class NavbarModule {}
