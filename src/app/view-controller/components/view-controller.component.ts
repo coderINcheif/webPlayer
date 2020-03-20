7559e6b48f86656399767f6ea925dc2ef75f56e2
planetred-cc <pandeyhimanshu709@gmail.com>
2020-03-20 14:04:04 +0530

    completed search bar

--- src/app/view-controller/components/view-controller.component.ts
+++ src/app/view-controller/components/view-controller.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-controller',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-controller.component.scss']
 })
 export class ViewControllerComponent implements OnInit {
+  constructor(private _router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  routeIncludes(url: string): boolean {
+    return this._router.url.includes(url);
   }
 
+  ngOnInit() {}
 }
--- src/app/view-controller/view-controller.module.ts
+++ src/app/view-controller/view-controller.module.ts
@@ -1,11 +1,13 @@
+import { RouterModule } from '@angular/router';
 import { ViewControllerComponent } from './components/view-controller.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationButtonsComponent } from './navigation-buttons/navigation-buttons.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
-  declarations: [ViewControllerComponent, NavigationButtonsComponent],
-  imports: [CommonModule],
+  declarations: [ViewControllerComponent, NavigationButtonsComponent, SearchBarComponent],
+  imports: [CommonModule, RouterModule],
   exports: [ViewControllerComponent]
 })
 export class ViewControllerModule {}
