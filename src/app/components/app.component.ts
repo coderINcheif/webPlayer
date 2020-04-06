f3ebdd03fa425df9c39e4ac9d9a9abb1b511373c
planetred-cc <pandeyhimanshu709@gmail.com>
2020-04-06 21:20:02 +0530

    minor changes

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'webPlayer';
+
+  updateScroll(element: HTMLElement) {
+    // console.log(element.scrollTop);
+  }
 }
--- src/app/explore/genere-music/genere-music.component.ts
+++ src/app/explore/genere-music/genere-music.component.ts
@@ -2,14 +2,22 @@ import { ColorService } from './../shared/services/color.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { GenereMusicService } from './services/genere-music.service';
 import { ParameterService } from './../shared/services/parameter.service';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-explore-genere-music',
   templateUrl: './genere-music.component.html',
   styleUrls: ['./genere-music.component.scss'],
 })
-export class GenereMusicComponent implements OnInit {
+export class GenereMusicComponent implements AfterViewInit, OnInit {
+  @ViewChild('heading', { static: true, read: ElementRef }) heading: ElementRef;
+
   title: string;
   color: string;
   constructor(
@@ -29,5 +37,12 @@ export class GenereMusicComponent implements OnInit {
       const genere = params.get('genere');
       this.title = this.genereMusicService.getTitle(genere);
     });
+  }
+
+  ngAfterViewInit() {
+    // console.log(this.heading.nativeElement.offsetTop);
+    // setTimeout(() => {
+    //   this.heading.nativeElement.style.transform = 'translateY(30px)';
+    // }, 3000);
   }
 }
