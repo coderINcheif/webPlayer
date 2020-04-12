bc3aa8acd9676077ff2efa636b64928c2dd262ac
planetred-cc <pandeyhimanshu709@gmail.com>
2020-04-12 13:21:55 +0530

    playlist banner in playlist detail is now centered

--- src/app/playlist-detail/components/playlist-detail.component.ts
+++ src/app/playlist-detail/components/playlist-detail.component.ts
@@ -1,6 +1,14 @@
 import { PlaylistDetailInterface } from './../interfaces/playlist-detail.interface';
 import { PlaylistDetailService } from './services/playlist-detail.service';
 import { Component, OnInit } from '@angular/core';
+import { ElementArrayFinder } from 'protractor';
+
+interface LinkedScrolling {
+  element: HTMLElement;
+  elementClass: string;
+  linked: HTMLElement;
+  linkedClass: string;
+}
 
 @Component({
   selector: 'app-playlist-detail',
@@ -10,9 +18,23 @@ import { Component, OnInit } from '@angular/core';
 export class PlaylistDetailComponent implements OnInit {
   playlist: PlaylistDetailInterface;
   gradientOpacity = 0.09;
+  prevScrollPos = { banner: 0, 'track-list': 0 };
+
   constructor(private playlistDetail: PlaylistDetailService) {}
 
   ngOnInit() {
     this.playlist = this.playlistDetail.playlist;
+  }
+
+  linkedScrolling({
+    element,
+    elementClass,
+    linked,
+    linkedClass,
+  }: LinkedScrolling) {
+    const scrollDist = element.scrollTop - this.prevScrollPos[elementClass];
+    linked.scrollTop += scrollDist;
+    this.prevScrollPos[elementClass] = element.scrollTop;
+    this.prevScrollPos[linkedClass] = linked.scrollTop;
   }
 }
