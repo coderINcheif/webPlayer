c21f3334053d75b9521d8650fcc8d556fe9937e2
planetred-cc <pandeyhimanshu709@gmail.com>
2020-05-23 16:20:32 +0530

    added jwt token authentication

--- backend/authentication/api/urls.py
+++ backend/authentication/api/urls.py
@@ -1,9 +1,14 @@
 from rest_framework import routers
 from django.conf.urls import url
 from . import views
+from rest_framework_simplejwt import views as simplejwt_views
 
 router = routers.DefaultRouter()
 
 router.register('users', views.UserViewSet, basename='user')
 
-urlpatterns = router.urls
+urlpatterns = [
+    url('login/', simplejwt_views.TokenObtainPairView.as_view(), name='auth-login')
+]
+
+urlpatterns += router.urls
