203ec4554171f867df731e45d31d13060d7099ad
planetred-cc <pandeyhimanshu709@gmail.com>
2020-05-23 15:47:29 +0530

    minor refactoring

--- backend/authentication/api/serializers.py
+++ backend/authentication/api/serializers.py
@@ -6,79 +6,74 @@ from django.core import exceptions
 
 class UserSerializer(serializers.ModelSerializer):
 
-    password2 = serializers.CharField(
-        write_only=True,
-        label='Confirm password'
-    )
+    confirm_password = serializers.CharField(write_only=True)
 
-    def validate_password(self, value, *args, **kwargs):
-        try:
-            validate_password(password=value)
-            return value
-        except exceptions.ValidationError as e:
-            raise serializers.ValidationError({"password": e.error_list})
+    class PasswordConfirmationError(Exception):
+        message = "Passwords do not match"
 
     def validate(self, attrs):
         validated_data = super().validate(attrs)
-        if validated_data['password2'] != validated_data['password']:
-            raise serializers.ValidationError(
-                {'password2': ["Passwords do not match"]}
-            )
-        return validated_data
+        password = validated_data.get('password')
+        password2 = validated_data.get('confirm_password')
+
+        try:
+            validate_password(password=password)
+            if password != password2:
+                raise UserSerializer.PasswordConfirmationError
+            return validated_data
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({"password": e.error_list})
+        except UserSerializer.PasswordConfirmationError as e:
+            raise serializers.ValidationError({'confirm_password': e.message})
 
     def create(self, validated_data):
-        password = validated_data['password']
-        email = validated_data['email']
-        first_name = validated_data['first_name']
-        last_name = validated_data['last_name']
-
-        user = auth_models.CustomUser()
-        user.email = email
-        user.first_name = first_name
-        user.last_name = last_name
-        user.set_password(password)
-        user.save()
+        validated_data.pop('confirm_password')
+        user = auth_models.CustomUser.objects.create(**validated_data)
         return user
 
     class Meta:
         model = auth_models.CustomUser
-        fields = ('first_name', 'last_name', 'email', 'password', 'password2')
-        extra_kwargs = {
-            'password': {'write_only': True},
-        }
+        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'first_name', 'last_name',
+                  'email', 'password', 'confirm_password')
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = auth_models.CustomUser
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('id', 'first_name', 'last_name', 'email')
 
 
 class ResetPasswordSerializer(serializers.ModelSerializer):
 
-    old_password = serializers.CharField(write_only=True)
+    current_password = serializers.CharField(write_only=True)
     new_password = serializers.CharField(write_only=True)
 
-    def update(self, instance, validated_data):
-        old_password = validated_data.get('old_password')
+    class InvalidPasswordError(Exception):
+        message = "Incorrect Password"
+
+    def validate(self, *args, **kwargs):
+        validated_data = super().validate(*args, **kwargs)
+        current_password = validated_data.get('current_password')
         new_password = validated_data.get('new_password')
 
-        if not instance.check_password(old_password):
-            raise serializers.ValidationError(
-                {'old_password': ["Password did not match"]}
-            )
-
         try:
+            if not self.instance.check_password(current_password):
+                raise ResetPasswordSerializer.InvalidPasswordError
             validate_password(password=new_password, user=self.instance)
-            instance.set_password(new_password)
-            instance.save()
-            return instance
+            return validated_data
         except exceptions.ValidationError as e:
-            raise serializers.ValidationError(
-                {'new_password': e.error_list}
-            )
+            raise serializers.ValidationError({'new_password': e.error_list})
+        except self.InvalidPasswordError as e:
+            raise serializers.ValidationError({'current_password': e.message})
+
+    def update(self, instance, validated_data):
+        new_password = validated_data.get('new_password')
+        instance.set_password(new_password)
+        instance.save()
+        return instance
 
     class Meta:
         model = auth_models.CustomUser
-        fields = ('old_password', 'new_password',)
+        fields = ('current_password', 'new_password',)
--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(max_length=254, null=False, unique=True)
+    username = models.CharField(
+        max_length=254, null=True, unique=False, blank=True
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
