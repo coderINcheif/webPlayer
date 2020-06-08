db14017bfbe152fdf9cf1a1d1762364ce4c6656e
planetred-cc <pandeyhimanshu709@gmail.com>
2020-06-09 00:03:41 +0530

    created playlist database

--- backend/playlist/models.py
+++ backend/playlist/models.py
@@ -1,3 +1,23 @@
+# models
+import song.models as song_models
+
+
+# django utils
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    songs = models.ManyToManyField(song_models.Song, through='PlaylistSong')
+
+
+class PlaylistSong(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    song = models.ForeignKey(song_models.Song, on_delete=models.CASCADE)
