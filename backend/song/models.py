d1188381045ad3dc3fe2fd83b9caa36cfb0a30c0
planetred-cc <pandeyhimanshu709@gmail.com>
2020-06-16 17:38:13 +0530

    created song generes

--- backend/song/models.py
+++ backend/song/models.py
@@ -3,5 +3,15 @@ from django.db import models
 # Create your models here.
 
 
+class Genere(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+
 class Song(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
+    genere = models.ManyToManyField(Genere, through='SongGenere')
+
+
+class SongGenere(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    genere = models.ForeignKey(Genere, on_delete=models.CASCADE)
